219b992cbfc05788f690b9a0e35e854bdfe98215
Corentin Cordonnier <corentincordonnier@hotmail.fr>
2020-07-01 18:46:15 +0200

    Ajout de Materialize et création d'un premier template

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,30 +9,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController{
 	/**
 	 * [DisplayHome description]
-	 * @Route("/")
+	 * @Route("/", name="home")
 	 */
 	public function displayHome(){
-		return new Response("hello you're on the future homepage !");
+		return $this->render("home/show.html.twig");
 	}
 
 	/**
 	 * [showPicture description]
-	 * @Route("/picture/{slug}")
+	 * @Route("/picture/{slug}", name="picture.display")
 	 */
 	public function showPicture($slug){
-		/*dump($slug);*/
+		dump($slug);
 		return $this->render("home/display.html.twig", [
 			'picture' => ucwords(str_replace('-',' ',$slug))]);
 
 	}
 
-	/**
-	 * [test description]
-	 * @Route("/test")
-	 */
-	public function test(){
-		return new Response("test !");
-	}
+	
 }
 
 ?>
